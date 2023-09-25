6deff2867e395fcb272a049de0816fb0f656bcca
stalegjelsten <stalegjelsten@gmail.com>
2023-09-25 10:56:07 +0200

    working testpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Læringsmål</h1>
+<ul>
+	<li>Mål 1</li>
+	<li>Mål 2</li>
+</ul>
+
+<h1>Tidsplan</h1>
+
+<style>
+	:global(html) {
+		height: 100vh;
+		width: 100vw;
+		font-size: 62.5%;
+	}
+	h1 {
+		font-size: 150%;
+	}
+</style>
